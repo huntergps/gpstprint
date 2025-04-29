152c9800b0dc2678d98e5b9bd899b0bbbc6e1347
Elmer Salazar <huntergps79@yahoo.es>
2025-04-29 13:46:31 -0500

    Commit Inicial clonando de QZ Try

---
+++ src/qz/build/provision/params/Type.java
@@ -0,0 +1,23 @@
+package qz.build.provision.params;
+
+import java.util.Locale;
+
+public enum Type {
+    SCRIPT,
+    SOFTWARE,
+    REMOVER, // QZ Tray remover
+    CA,
+    CERT,
+    CONF,
+    PROPERTY,
+    PREFERENCE;
+
+    public static Type parse(String input) {
+        return EnumParser.parse(Type.class, input);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString().toLowerCase(Locale.ENGLISH);
+    }
+}
---
+++ src/qz/ui/LogDialog.java
@@ -0,0 +1,101 @@
+package qz.ui;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.Filter;
+import org.apache.logging.log4j.core.appender.WriterAppender;
+import org.apache.logging.log4j.core.filter.ThresholdFilter;
+import org.apache.logging.log4j.core.layout.PatternLayout;
+import qz.ui.component.IconCache;
+import qz.ui.component.LinkLabel;
+import qz.utils.FileUtilities;
+import qz.utils.LoggerUtilities;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.StringWriter;
+
+/**
+ * Created by Tres on 2/26/2015.
+ */
+public class LogDialog extends BasicDialog {
+
+    private final int ROWS = 20;
+    private final int COLS = 80;
+
+    private JScrollPane logPane;
+    private JTextArea logArea;
+
+    private JButton clearButton;
+
+    private WriterAppender logStream;
+
+
+    public LogDialog(JMenuItem caller, IconCache iconCache) {
+        super(caller, iconCache);
+        initComponents();
+    }
+
+    public void initComponents() {
+        int defaultFontSize = new JLabel().getFont().getSize();
+        LinkLabel logDirLabel = new LinkLabel(FileUtilities.USER_DIR + File.separator);
+        logDirLabel.setLinkLocation(new File(FileUtilities.USER_DIR + File.separator));
+        setHeader(logDirLabel);
+
+        StringWriter writeTarget = new StringWriter() {
+            @Override
+            public void flush() {
+                SwingUtilities.invokeLater(() -> {
+                    logArea.append(toString());
+                    logPane.getVerticalScrollBar().setValue(logPane.getVerticalScrollBar().getMaximum());
+                });
+            }
+        };
+
+        logArea = new JTextArea(ROWS, COLS);
+        logArea.setEditable(false);
+        logArea.setLineWrap(true);
+        logArea.setWrapStyleWord(true);
+        logArea.setFont(new Font("", Font.PLAIN, defaultFontSize)); //force fallback font for character support
+
+        // TODO:  Fix button panel resizing issues
+        clearButton = addPanelButton("Clear", IconCache.Icon.DELETE_ICON, KeyEvent.VK_L);
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                logArea.setText(null);
+
+                writeTarget.getBuffer().setLength(0);
+            }
+        });
+
+        logPane = new JScrollPane(logArea);
+        setContent(logPane, true);
+        setResizable(true);
+
+        // add new appender to Log4J just for text area
+        logStream = WriterAppender.newBuilder()
+                .setName("ui-dialog")
+                .setLayout(PatternLayout.newBuilder().withPattern("[%p] %d{ISO8601} @ %c:%L%n\t%m%n").build())
+                .setFilter(ThresholdFilter.createFilter(Level.TRACE, Filter.Result.ACCEPT, Filter.Result.DENY))
+                .setTarget(writeTarget)
+                .build();
+        logStream.start();
+    }
+
+    @Override
+    public void setVisible(boolean visible) {
+        if (visible) {
+            logArea.setText(null);
+            LoggerUtilities.getRootLogger().addAppender(logStream);
+        } else {
+            LoggerUtilities.getRootLogger().removeAppender(logStream);
+        }
+
+        super.setVisible(visible);
+    }
+
+}
